94ca769bb665c5d65440c096b213e05a845be7af
Teo Jungo <massaqui2021@gmail.com>
2025-03-16 08:17:53 +0100

    Building the project

---
+++ business/models.py
@@ -0,0 +1,20 @@
+import uuid
+from django.db import models
+
+
+class Business(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    social_midea = models.CharField(max_length=500)
+    nif = models.CharField(max_length=15)
+    phone = models.CharField(max_length=15,default=True)
+    is_active = models.CharField(max_length=20,default=True,null=False)
+    address = models.CharField(max_length=255,default=True,null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name_plural = 'businesses'
+        
+    def __str__(self):
+        return self.social_midea
+    
