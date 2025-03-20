943b6641ad52eb4c998e900f4b6646984f26b28c
Teo Jungo <84847200+TeuJungo@users.noreply.github.com>
2025-03-20 06:07:14 +0100

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+                
 # Configuração do Schema View
 schema_view = get_schema_view(
     openapi.Info(
